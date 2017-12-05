b256cf9d400c4db3d5cabe292138ee4faddef040
Lekcrisna <33767571+Lekcrisna@users.noreply.github.com>
2017-12-05 15:36:18 -0800

    Initial commit

---
+++ Crisna-Guessgame.py
@@ -0,0 +1,22 @@
+
+
+
+# print("Guess a number between 1 and 50")
+# response = ""
+# while response != "50":
+#     response = input("Say \"50\"")
+num = input("Give me a number")
+
+# import random
+# number = (random.randint(0,50))
+
+# def grade_calc(number):
+#     if number <= 50:
+#         return "Wrong man"
+
+
+# 1) generate a number
+# 2)get a number (input) from the user
+# 3)compare number to input
+# 4)add "Higher" or "lower"
+# 5)add 5 guesses
